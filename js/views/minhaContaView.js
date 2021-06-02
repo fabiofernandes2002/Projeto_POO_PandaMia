628642f1eea6fa10cd83c7ceade078b07d283e70
Fabio Fernandes <fabiofernandes2002@gmail.com>
2021-06-02 18:32:15 +0100

    algumas coisas modificadas

--- js/views/minhaContaView.js
+++ js/views/minhaContaView.js
@@ -8,22 +8,28 @@ export default class minhaContaView {
 
         // gestão do form de dados de acesso
         this.formDataAccess = document.querySelector("#formDataAccess")
-        this.emailNew = document.querySelector("#txtEmailNew")
-        this.passwordCurrent = document.querySelector("#txtPasswordCurrent")
-        this.passwordNew = document.querySelector("#txtPasswordNew")
-        this.repeatPasswordNew = document.querySelector("#txtRepeatPasswordNew")
+        this.emailNew = document.querySelector("#txtEmailNew").value;
+        this.passwordCurrent = document.querySelector("#txtPasswordCurrent").value;
+        this.passwordNew = document.querySelector("#txtPasswordNew").value;
+        this.repeatPasswordNew = document.querySelector("#txtRepeatPasswordNew").value
         this.btnSave = document.querySelector("#btnSave")
         this.dataAccessMessage = document.querySelector("#dataAccessMessage")
         this.bindFormDataAccess()
 
-        // gestão do form de dados pessoais
-        this.formPersonalData = document.querySelector("#formPersonalData")
-        this.usernameNew = document.querySelector("#txtUsernameNew")
-        this.addressNew = document.querySelector("#txtAddressNew")
-        this.cityNew = document.querySelector("#txtCityNew")
-        this.postalCodeNew = document.querySelector("#txtPostalCodeNew")
-        this.btnSave = document.querySelector("#btnSave")
-        this.personalDataMessage = document.querySelector("#personalDataMessage")
-        this.bindFormPersonalData()
+    }
+
+    bindFormDataAccess(){
+        this.formDataAccess.addEventListener("submit", event =>{
+            event.preventDefault();
+            try {
+                if (this.password) {
+                    
+                } else {
+                    
+                }
+            } catch (error) {
+                
+            }
+        })
     }
 }
