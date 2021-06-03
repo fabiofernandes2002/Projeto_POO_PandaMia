2e093e4ee61a83d9bb2277ddfd6174f60b66d7ad
tomasnborges <tomas.borges.7007@gmail.com>
2021-06-04 00:38:18 +0100

    Cria a pagina dos jogos

--- js/myScript.js
+++ js/myScript.js
@@ -2,3 +2,29 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 /* Dar refresh à página sempre que damos resize */
 window.onresize = function(){ location.reload(); } 
+
+/* Event Listeners para o catálogo de jogos */
+let cardOne=document.getElementById("card1")
+let cardTwo=document.getElementById("card2")
+
+    function hideCard(e){
+        /* Apaga todos os nodes childs que estão dentro do elemento e */
+    e.innerHTML=""
+    e.innerHTML+=`<div class="card-body">
+    <h2 class="card-text text-center">QUIZ 1</h2>
+    <hr>
+    <h2 class="card-text text-center">QUIZ 2</h2>
+    <hr>
+    <h2 class="card-text text-center">QUIZ 3</h2>
+    </div>`
+    setTimeout(function(){
+        window.addEventListener('click', function(e){
+            
+        })
+    }, 1)
+    
+    }
+    
+  
+
+    cardOne.addEventListener('click', function(){hideCard(cardOne)})
