674ce392668724bf1a53343de44abcb302d68ed7
fabiofernandes2002 <75033072+fabiofernandes2002@users.noreply.github.com>
2021-06-09 15:30:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -111,9 +111,76 @@ class App {
 
         ];
         
-
+        const quizGames = [
+            {
+                id: "1",
+                name: "Quiz 1",
+                description: "Será que consegues acertar? Testa o seu conhecimento sobre a COVID-19!"
+            ,
+            questions: [
+                {
+                    title: "Qual é o tempo recomendado para lavar as mãos?",
+                    respostaCerta: "20 segundos",
+                    respostaErrada: "10 segundos",
+                    respostaErrada: "15 segundos"
+                },
+                {
+                    title: "De que forma se transmite o COVID-19?",
+                    respostaCerta: "Com superfícies, objetos e pessoas contaminadas",
+                    respostaErrada: "Desinfetando as mãos",
+                    respostaErrada: "Utilizando máscara nova"
+                },
+                {
+                    title: "Durante quantas horas a utilização da mesma máscara descartável é segura?",
+                    respostaCerta: "4 horas",
+                    respostaErrada: "2 horas",
+                    respostaErrada: "8 horas"
+                },
+                {
+                    title: "Qual a distância recomendada para se permanecer perto de outras pessoas?",
+                    respostaCerta: "1,5 metros",
+                    respostaErrada: "1 metro",
+                    respostaErrada: "0,5 metro"
+                },
+                {
+                    title: "Qual o período de incubação do vírus?",
+                    respostaCerta: "1 a 14 dias",
+                    respostaErrada: "1 a 20 dias",
+                    respostaErrada: " 1 a 7 dias"
+                },
+                {
+                    title: "O que podes fazer durante o período de isolamento?",
+                    respostaCerta: "Utilizar a divisão da casa onde estás isolado",
+                    respostaErrada: "Sair de casa, estar a conviver com os amigos, estar junto com as outras pessoas da família",
+                    respostaErrada: "Ter visitas de amigos e familiares"
+                },
+                {
+                    title: "Que medidas devo tomar se tiver sintomas que possam estar associados a COVID-19?",
+                    respostaCerta: "Ligar para o número de telefone SNS 24 para evitar o contacto pessoal e obter informações mais concretas sobre como devo proceder",
+                    respostaErrada: "Ir imediatamente ao hospital, pois pode ser grave",
+                    respostaErrada: "Permanecer em casa sem qualquer avaliação médica"
+                },
+                {
+                    title: "Qual a sequência correta de desinfeção das mãos?",
+                    respostaCerta: "Molhar as mãos; Aplicar sabão que cobre toda a superfície das mãos; Esfregar as palmas das mãos e entrelaçar os dedos; Esfregar os polegares, as costas das mãos e os pulsos;",
+                    respostaErrada: "Enxaguar as mãos com água; Secar com um toalhete descartável",
+                    respostaErrada: "Lavar as mãos só com água"
+                },
+                {
+                    title: "Como se deve colocar a máscara?",
+                    respostaCerta: "Lavar as mãos com água e sabão ou desinfetar com álcool gel; Colocar a máscara na posição correta; Segurar a máscara pelos elásticos e adaptar à orelha; Ajustar justo ao nariz e queixo sem tocar na face da máscara",
+                    respostaErrada: "Lavar as mãos e depois colocar a máscara",
+                    respostaErrada: "Desinfetar as mãos com álcool gel; Segurar na face da máscara e depois pôr na posição correta"
+                }
+            
+            ]
+        }
+        ]
         
-
+        // Load the fixtures in case there is no data in the local storage 
+        if (!localStorage.games) {
+            localStorage.setItem('quizGames', JSON.stringify(quizGames));
+        }
         
         if (!localStorage.users) {
             localStorage.setItem("users", JSON.stringify(users));
